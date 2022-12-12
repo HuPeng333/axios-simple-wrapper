d5e6aae0562cdb718c5913655dc3bf702c382eea
DeSen Xu <2237803016@qq.com>
2022-12-12 16:56:56 +0800

    fix: 修复了POST参数会忽略掉属性值为0的属性

--- src/util/requestUtils.ts
+++ src/util/requestUtils.ts
@@ -2,15 +2,15 @@
  * 构建post请求的参数
  * @param postParam {Object} post请求参数
  */
-export const parseObjToPostParam = (postParam?: Record<string, string | number>) => {
+export const parseObjToPostParam = (postParam?: Record<string, string | number | undefined>) => {
   if (!postParam || typeof postParam != 'object') {
     return ''
   }
   let params = ''
   Object.keys(postParam).forEach(key => {
     const value = postParam[key]
-    if (value) {
-      params += (encodeURIComponent(key) + '=' + encodeURIComponent(postParam[key]) + '&')
+    if (value !== undefined) {
+      params += (encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&')
     }
   })
   // 替换最后一个多余的&符号
--- test/util/requestUtil.test.ts
+++ test/util/requestUtil.test.ts
@@ -1,6 +1,6 @@
 import {parseObjToPostParam} from "../../src/util/requestUtils";
 
 test('test param', () => {
-  const param = parseObjToPostParam({a: 1, b: undefined, c: 'abc', d: null})
-  expect(param).toBe('a=1&c=abc')
+  const param = parseObjToPostParam({a: 1, b: 0, c: 'abc', d: undefined})
+  expect(param).toBe('a=1&b=0&c=abc')
 })
